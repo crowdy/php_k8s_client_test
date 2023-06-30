394541924e6821aa9508f12db2dd52ae1a72be2d
tonghyun kim <tonghyun-kim@gmo.jp>
2023-06-30 22:51:19 +0900

    create and delete pod

--- test.php
+++ test.php
@@ -2,7 +2,11 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Maclof\Kubernetes\Client;
-use GuzzleHttp\Client as GuzzleClient;
+use Maclof\Kubernetes\Models\Job;
+use Maclof\Kubernetes\Models\Pod;
+use Maclof\Kubernetes\Models\Service;
+use Maclof\Kubernetes\RepositoryRegistry;
+
 
 $client = new Client([
     'master' => 'https://172.29.8.65:6443/',
@@ -17,18 +21,67 @@ $pods = $client->pods()->setLabelSelector([
 
 // Both setLabelSelector and setFieldSelector can take an optional
 // second parameter which lets you define inequality based selectors (ie using the != operator)
+
+/** @var Maclof\Kubernetes\Collections\PodCollection $pods */
 $pods = $client->pods()->find();
+
+
+
+echo "\n\nlist pods" . PHP_EOL;
+$client->pods()->find()->each(function (Pod $pod) {
+    echo $pod->getMetadata('name') . ' / ' . $pod->getMetadata('label') . PHP_EOL;
+});
+
+// create pod
+
+if (! $client->pods()->exists('tkim-test')) {
+    echo "\n\npod does not exist. creating" . PHP_EOL;
+    $client->pods()->create(new Pod([
+        'metadata' => [
+            'name' => 'tkim-test',
+            'labels' => [
+                'name' => 'tkim-test',
+            ],
+        ],
+        'spec'     => [
+            'containers' => [
+                [
+                    'name'  => 'test',
+                    'image' => 'nginx',
+                ],
+            ],
+        ],
+    ]));
+} else {
+
+    echo "\n\npod exists. deleting" . PHP_EOL;
+    $client->pods()->deleteByName('tkim-test');
+}
+
+
+// list services
+echo "\n\nlist services" . PHP_EOL;
+$client->services()->find()->each(function (Service $job) {
+    echo $job->getMetadata('name') . PHP_EOL;
+});
+
+// list jobs
+echo "\n\nlist jobs" . PHP_EOL;
+$client->jobs()->find()->each(function (Job $job) {
+    echo $job->getMetadata('name') . PHP_EOL;
+});
+
 $pods = $client->pods()->setLabelSelector([
-    'name'    => 'test'],
+    'name'    => 'tkim-test'],
     ['env'     =>  'staging']
 )->find();
 
 // Find pods by field selector
 $pods = $client->pods()->setFieldSelector([
-    'metadata.name' => 'test',
+    'metadata.name' => 'tkim-test',
 ])->find();
 
 // Find first pod with label selector (same for field selector)
 $pod = $client->pods()->setLabelSelector([
-    'name' => 'test',
+    'name' => 'tkim-test',
 ])->first();
